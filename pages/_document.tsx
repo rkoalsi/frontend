60ae22dedde68158820544e1dad2ffc5f944a9b9
rkoalsi <57904969+rkoalsi@users.noreply.github.com>
2026-02-18 13:21:51 +0530

    replacing google tag manager

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,17 +11,17 @@ export default function MyDocument(props: DocumentProps & DocumentHeadTagsProps)
   return (
     <Html lang="en" className={roboto.className}>
       <Head>
-        {/* Google Tag Manager */}
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZQCK1YFZRQ" />
         <script
           dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5HHPCPFK');`,
+            __html: `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-ZQCK1YFZRQ');`,
           }}
         />
-        {/* End Google Tag Manager */}
+        {/* End Google tag (gtag.js) */}
         {/* PWA primary color */}
         <meta name="theme-color" content={theme.palette.primary.main} />
         <link rel="shortcut icon" href="/favicon.ico" />
@@ -29,16 +29,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         <DocumentHeadTags {...props} />
       </Head>
       <body>
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-5HHPCPFK"
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          />
-        </noscript>
-        {/* End Google Tag Manager (noscript) */}
         <Main />
         <NextScript />
       </body>
